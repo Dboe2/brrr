7c820f2670fe92563094984675005a8939900e19
dkirby-ms <saitcho@outlook.com>
2021-01-31 15:15:42 -0600

    api.py

--- pyetrade-main/webapi/api.py
+++ pyetrade-main/webapi/api.py
@@ -6,6 +6,11 @@ from azure.identity import DefaultAzureCredential
 KVUri = f"https://brrkeys1.vault.azure.net"
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
+consumer_key = client.get_secret('etradeSandboxKey').value
+consumer_secret = client.get_secret('etradeSandboxSecret').value
+
+oauth = pyetrade.ETradeOAuth(consumer_key, consumer_secret)
+tokenurl = oauth.get_request_token()
 
 app = Flask(__name__)
 
@@ -14,11 +19,16 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Pyetrade API</h1><p>This site is a prototype API for pyetrade.</p>"
+    return "<h1>Brrr API</h1><p>This site is a prototype API for pyetrade.</p>"
 
-@app.route('/api/v1/test', methods=['GET'])
+@app.route('/api/v1/accounts/list', methods=['GET'])
 def api_accounts_list():
-    retrievedSecret = client.get_secret('etradeSandboxKey')
-    return retrievedSecret.value
+    accounts = pyetrade.ETradeAccounts(
+        consumer_key,
+        consumer_secret,
+        tokens['oauth_token'],
+        tokens['oauth_token_secret']
+    )
+    return print(accounts.list_accounts())
 
 app.run(host="0.0.0.0", port=int("5000"), debug=True)
